280caea79a27ca15462e1c2085d386d673c0a424
GolamMuid <golammuid@gmail.com>
2022-12-13 21:10:22 +0600

    added course delete and edit

--- routes/courses.js
+++ routes/courses.js
@@ -1,19 +1,30 @@
 const express = require("express");
 
-const { getCourses, getCourse, addCourse } = require("../controllers/courses");
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  deleteCourse,
+  updateCourse,
+} = require("../controllers/courses");
 
-const Course = require('../models/Course')
-const advancedResults = require('../middlewares/advancedResults')
+const Course = require("../models/Course");
+const advancedResults = require("../middlewares/advancedResults");
 
-const { protect } = require('../middlewares/auth')
+const { protect } = require("../middlewares/auth");
 
 const router = express.Router({ mergeParams: true });
 
-router.route("/").get(advancedResults(Course, {
-  path: "bootcamp",
-  select: "name description",
-}), getCourses);
+router.route("/").get(
+  advancedResults(Course, {
+    path: "bootcamp",
+    select: "name description",
+  }),
+  getCourses
+);
 router.route("/:id").get(getCourse);
 router.route("/").post(protect, addCourse);
+router.route("/:id").delete(protect, deleteCourse);
+router.route("/:id").put(protect, updateCourse);
 
 module.exports = router;
