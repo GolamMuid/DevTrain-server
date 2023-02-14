90b9ab2ab32d1b77354a160690a75daeeebc104a
Md. Golam Muid Talukder <93812468+GolamMuid@users.noreply.github.com>
2023-02-14 09:40:13 +0600

    Removed colors

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-const colors = require("colors");
 
 const connectDB = async () => {
   mongoose.set("strictQuery", false);
   const conn = await mongoose.connect(process.env.MONGO_URI, {});
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`.yellow.underline);
+  console.log(`MongoDB Connected: ${conn.connection.host}`);
 };
 
 module.exports = connectDB;
